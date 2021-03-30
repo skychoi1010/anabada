fce65261a9b653308d3da1aa0f85528ea5b96726
skychoi <skychoi1010@naver.com>
2021-03-31 00:30:45 +0900

    fading toolbar in progress

--- app/src/main/java/com/example/anabada/BoardDetailActivity.kt
+++ app/src/main/java/com/example/anabada/BoardDetailActivity.kt
@@ -2,7 +2,6 @@ package com.example.anabada
 
 import android.R
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
@@ -10,16 +9,20 @@ import android.view.View.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.anabada.databinding.ActivityBoardDetailBinding
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import kotlinx.android.synthetic.main.activity_board_detail.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import kotlin.math.abs
 
 
 class BoardDetailActivity : AppCompatActivity() {
@@ -64,6 +67,32 @@ class BoardDetailActivity : AppCompatActivity() {
         })
          */
 
+        binding.appBarLayout.addOnOffsetChangedListener(object :
+            AppBarLayout.OnOffsetChangedListener {
+            override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+                var verticalOffsetAbs = abs(verticalOffset)
+                val view: View = View(this@BoardDetailActivity)
+                var halfScrollRange = (binding.appBarLayout.totalScrollRange.times(0.5f))
+                var ratio = verticalOffset / halfScrollRange
+                ratio = 0f.coerceAtLeast(1f.coerceAtMost(ratio))
+                if (appBarLayout != null) {
+                    appBarLayout.background.alpha = (ratio * 255).toInt()
+                }
+                ViewCompat.setAlpha(view, ratio)
+            }
+        })
+        class FadingViewOffsetListener(private val mView: View) :
+            OnOffsetChangedListener {
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                var verticalOffset = verticalOffset
+                verticalOffset = Math.abs(verticalOffset)
+                val halfScrollRange: Float = (appBarLayout.totalScrollRange * 0.5f)
+                var ratio = verticalOffset.toFloat() / halfScrollRange
+                ratio = Math.max(0f, Math.min(1f, ratio))
+                ViewCompat.setAlpha(mView, ratio)
+            }
+        }
+        /*
         setSupportActionBar(binding.toolbar)
         if (supportActionBar != null) {
             //supportActionBar!!.setDisplayShowTitleEnabled(false)
@@ -73,14 +102,15 @@ class BoardDetailActivity : AppCompatActivity() {
             supportActionBar!!.title = title
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
                 binding.toolbar.elevation = 1F
-                toolbar.setBackgroundColor(Color.GRAY)
+                toolbar.setBackgroundColor(Color.TRANSPARENT)
             }
             val statusBarHeight = getStatusBarHeight()
-            binding.nsvBoardDetail.setPadding(1, 180 + statusBarHeight, 1, 0)
+            binding.nsvBoardDetail.setPadding(0, 100 + statusBarHeight, 0, 0)
             val params = toolbar.layoutParams
-            params.height = statusBarHeight
+            params.height = 100 + statusBarHeight
             toolbar.layoutParams = params
         }
+        */
 
     }
 
@@ -96,20 +126,35 @@ class BoardDetailActivity : AppCompatActivity() {
 
         api.reqBoardDetail(id).enqueue(object : Callback<BoardDetailRes> {
             override fun onFailure(call: Call<BoardDetailRes>, t: Throwable) {
-                Toast.makeText(this@BoardDetailActivity, "board api\nFailed connection", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@BoardDetailActivity,
+                    "board api\nFailed connection",
+                    Toast.LENGTH_SHORT
+                ).show()
                 //end
             }
 
-            override fun onResponse(call: Call<BoardDetailRes>, response: Response<BoardDetailRes>) {
+            override fun onResponse(
+                call: Call<BoardDetailRes>,
+                response: Response<BoardDetailRes>
+            ) {
                 boardDetailRes = response.body()
                 when {
                     boardDetailRes?.resultCode == null -> {
                         //end
-                        Toast.makeText(this@BoardDetailActivity, "페이지를 불러오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@BoardDetailActivity,
+                            "페이지를 불러오는 데 실패했습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                     boardDetailRes?.board == null -> {
                         //end
-                        Toast.makeText(this@BoardDetailActivity, "게시글이 존재하지 않습니다", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@BoardDetailActivity,
+                            "게시글이 존재하지 않습니다",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                     else -> {
                         binding.tvBoardDetailContents.text = boardDetailRes?.board?.contents
@@ -117,7 +162,8 @@ class BoardDetailActivity : AppCompatActivity() {
                             .load(boardDetailRes?.board?.detailImg)
                             .apply(RequestOptions().placeholder(R.drawable.ic_dialog_alert))
                             .into(binding.ivBoardDetailImg)
-                        binding.tvBoardDetailComment.text = "댓글 " + boardDetailRes?.board?.commentCount.toString() + " > "
+                        binding.tvBoardDetailComment.text =
+                            "댓글 " + boardDetailRes?.board?.commentCount.toString() + " > "
                     }
                 }
             }
@@ -130,12 +176,18 @@ class BoardDetailActivity : AppCompatActivity() {
             this@BoardDetailActivity,
             LinearLayoutManager.VERTICAL
         )
-        ContextCompat.getDrawable(this@BoardDetailActivity, R.drawable.divider_horizontal_bright)?.let { dividerItemDecoration.setDrawable(it) }
+        ContextCompat.getDrawable(this@BoardDetailActivity, R.drawable.divider_horizontal_bright)?.let { dividerItemDecoration.setDrawable(
+            it
+        ) }
         binding.rvBoardDetailCommentsPrev.addItemDecoration(dividerItemDecoration)
 
-        commentsPrevRecyclerAdapter.setItemClickListener(object : CommentsPrevRecyclerAdapter.ItemClickListener {
+        commentsPrevRecyclerAdapter.setItemClickListener(object :
+            CommentsPrevRecyclerAdapter.ItemClickListener {
             override fun onClick(view: View, id: Int) {
-                Intent(this@BoardDetailActivity, BoardDetailActivity::class.java).apply { //TODO comment detail activity
+                Intent(
+                    this@BoardDetailActivity,
+                    BoardDetailActivity::class.java
+                ).apply { //TODO comment detail activity
                     putExtra("comment id", id)
                     startActivity(this)
                 }
@@ -153,10 +205,19 @@ class BoardDetailActivity : AppCompatActivity() {
     }
 
 
-    private fun callComments(pageNum: Int, api: ApiService, id: Int, binding: ActivityBoardDetailBinding) {
+    private fun callComments(
+        pageNum: Int,
+        api: ApiService,
+        id: Int,
+        binding: ActivityBoardDetailBinding
+    ) {
         api.reqComment(id, pageNum).enqueue(object : Callback<CommentRes> {
             override fun onFailure(call: Call<CommentRes>, t: Throwable) {
-                Toast.makeText(this@BoardDetailActivity, "comment api\nFailed connection", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@BoardDetailActivity,
+                    "comment api\nFailed connection",
+                    Toast.LENGTH_SHORT
+                ).show()
                 //end
             }
 
@@ -165,7 +226,11 @@ class BoardDetailActivity : AppCompatActivity() {
                 when {
                     commentRes?.success == null -> {
                         //end
-                        Toast.makeText(this@BoardDetailActivity, "페이지를 불러오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@BoardDetailActivity,
+                            "페이지를 불러오는 데 실패했습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                     commentRes?.comments?.isEmpty() == true -> {
                         //end
